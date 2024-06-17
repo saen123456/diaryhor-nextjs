540245935ac962a5f80481c9dff0be69c5185d14
Pipat Angkaew <pipiat0909737525@gmail.com>
2024-06-17 11:23:37 +0700

    fix style

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -29,6 +29,7 @@ const Navbar = () => {
             height={70}
             width={70}
             alt="My Logo"
+            className="rounded-lg"
           />
         </h1>
       </div>
@@ -52,7 +53,7 @@ const Navbar = () => {
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-white to-gray-200 text-black">
           {links.map(({ id, link, title }) => (
             <li
               key={id}
