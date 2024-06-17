d7ce192684cc339b55a4781865a53d2e2d916d6c
Pipat Angkaew <pipiat0909737525@gmail.com>
2024-06-17 11:19:21 +0700

    add: nav

--- pages/components/navbar.tsx
+++ pages/components/navbar.tsx
@@ -7,15 +7,15 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   const links = [
-    { id: 1, link: "home" },
-    { id: 2, link: "about" },
-    { id: 3, link: "portfolio" },
-    { id: 4, link: "experience" },
-    { id: 5, link: "contact" },
+    { id: 1, link: "/", title: "หน้าแรก" },
+    { id: 2, link: "about", title: "about" },
+    { id: 3, link: "portfolio", title: "portfolio" },
+    { id: 4, link: "experience", title: "experience" },
+    { id: 5, link: "contact", title: "contact" },
   ];
 
   return (
-    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed nav">
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-white fixed nav">
       <div>
         <h1 className="text-5xl font-signature ml-2">
           <a
@@ -34,12 +34,12 @@ const Navbar = () => {
       </div>
 
       <ul className="hidden md:flex">
-        {links.map(({ id, link }) => (
+        {links.map(({ id, link, title }) => (
           <li
             key={id}
-            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-black duration-200 link-underline"
           >
-            <Link href={`/${link}`}>{link}</Link>
+            <Link href={`/${link}`}>{title}</Link>
           </li>
         ))}
       </ul>
@@ -53,13 +53,13 @@ const Navbar = () => {
 
       {nav && (
         <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
-          {links.map(({ id, link }) => (
+          {links.map(({ id, link, title }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
               <Link onClick={() => setNav(!nav)} href={`/${link}`}>
-                {link}
+                {title}
               </Link>
             </li>
           ))}
